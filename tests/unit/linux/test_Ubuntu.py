a7c7e81996a1734737804d37e5e9ae36e255bc95
anejaalekh <anejaalekh@gmail.com>
2021-01-23 12:04:35 -0500

    Fix Review Comments

--- tests/unit/linux/test_Ubuntu.py
+++ tests/unit/linux/test_Ubuntu.py
@@ -10,10 +10,10 @@ from k8_vmware.vsphere.VM_Create import VM_Create
 class test_Ubuntu(TestCase):
 
     def setUp(self) -> None:
-        self.vm_create = VM_Create()
-        self.vm = self.vm_create.create()
-        self.vm_name = self.vm_create.vm_name
-        self.ubuntu = Ubuntu(self.vm_name)
+        self.vm_create  = VM_Create()
+        self.vm         = self.vm_create.create()
+        self.vm_name    = self.vm_create.vm_name
+        self.ubuntu     = Ubuntu(self.vm_name)
 
     def tearDown(self) -> None:
         self.vm.task().delete()
@@ -22,18 +22,17 @@ class test_Ubuntu(TestCase):
         assert self.ubuntu.vm_name == self.vm_name
 
     def test_vm(self):
-        with View_Soap_Calls():
-            assert self.ubuntu.vm().name() == self.vm_name
+        assert self.ubuntu.vm().name() == self.vm_name
+        assert self.ubuntu.exists()
 
-    def test_query(self):
+    def test_sample_properties_fetch(self):
         sdk = self.ubuntu.sdk
         vm = sdk.get_object(pyVmomi.vim.VirtualMachine, self.vm_name)
         assert vm.name == self.vm_name
 
         properties = ['parent', 'configStatus', 'config', 'tag']
-        with View_Soap_Calls(show_calls=True, show_xml=False):
-            result = sdk.get_objects_properties(pyVmomi.vim.VirtualMachine, [vm], properties)
-            self.assertIsNotNone(result.get(self.vm.id()).get("config"))
-            self.assertIsNotNone(result.get(self.vm.id()).get("parent"))
-            self.assertIsNotNone(result.get(self.vm.id()).get("configStatus"))
-            self.assertIsNotNone(result.get(self.vm.id()).get("tag"))
+        result = sdk.get_objects_properties(pyVmomi.vim.VirtualMachine, [vm], properties)
+        self.assertIsNotNone(result.get(self.vm.id()).get("config"))
+        self.assertIsNotNone(result.get(self.vm.id()).get("parent"))
+        self.assertIsNotNone(result.get(self.vm.id()).get("configStatus"))
+        self.assertIsNotNone(result.get(self.vm.id()).get("tag"))
