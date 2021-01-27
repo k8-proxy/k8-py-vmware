5ef9b8c7d63f67fe953545c4101838d415f7e7c1
samarthdd <samarth@smalldaytech.com>
2021-01-27 13:22:11 +0530

    refactor

--- tests/unit/vsphere/ova_utils/test_OVF_Handler.py
+++ tests/unit/vsphere/ova_utils/test_OVF_Handler.py
@@ -9,26 +9,11 @@ from k8_vmware.vsphere.ova_utils.OVA import OVA
 from k8_vmware.vsphere.ova_utils.OVF_Handler import OvfHandler
 from k8_vmware.vsphere.ova_utils.Web_Handle import WebHandle
 
-
 class test_OvfHandler(TestCase_OVA):
     def setUp(self) -> None:
         self.ova = OVA()
         self.ova.download_ova_file(self.url, self.ova_path)
         self.ova_handler = OvfHandler(self.ova_path)
-
-        self.host       =    self.ova.sdk.server_details().get('host')
-
-        ovfManager      =    self.ova.sdk.content().ovfManager
-        rp              =    self.ova.sdk.resource_pool()
-        ds              =    self.ova.sdk.datastore()
-        dc              =    self.ova.sdk.datacenter()
-        cisp            =    pyVmomi.vim.OvfManager.CreateImportSpecParams()
-        self.cisr       =    ovfManager.CreateImportSpec(self.ova_handler.get_descriptor(), rp, ds, cisp)
-        self.lease = rp.ImportVApp(self.cisr.importSpec, dc.vmFolder)
-
-    def test_set_spec(self):
-        self.ova_handler.set_spec(self.cisr)
-        assert self.ova_handler.spec is self.cisr
 
     def test_init(self):
         assert file_exists(self.ova_path)
@@ -53,8 +38,3 @@ class test_OvfHandler(TestCase_OVA):
     def test_get_device_url_failure_case(self):
         with self.assertRaises(Exception) as context:
             self.ova_handler.get_device_url(fileItem='fileItem',lease='lease')
-
-    # def test_upload_disk(self):
-    #     self.ova_handler.set_spec(self.cisr)
-    #     result = self.ova_handler.upload_disks(self.lease , self.host)
-    #     assert result is 0
